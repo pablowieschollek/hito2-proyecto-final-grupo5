f1887e57f266f6138b0bc168ac8e6e9e3b3950fe
pablowieschollek <p.i.w.andaur@gmail.com>
2023-03-11 14:43:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import MyContext from './MyContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {useState, useEffect} from 'react';
+//importación de vistas 
+import Home from './views/Home';
+import Login from './views/Login';
+import Registro from './views/Registro';
+import Perfil from './views/Perfil';
+import Formulario from './views/Formulario';
+import Galeria from './views/Galeria';
+import InfoProducto from './views/InfoProducto';
+import Favoritos from './views/Favoritos';
+import EnVenta from './views/EnVenta';
+import NotFound from './views/NotFound';
+//importación de componentes
+import Botones from './componentes/Botones';
+import Cards from './componentes/Cards';
+import Navbar from './componentes/Navbar';
+import Sidebar from './componentes/Sidebar';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const estadoGlobal = {pokemon, setPokemon}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyContext.Provider value={estadoGlobal}>
+        < BrowserRouter >
+          < Navbar />
+            < Routes>
+              < Route path="/" element={< Home />} />
+              < Route path="/login" element={< Login />} />
+              < Route path="/registro" element={< Registro />} />
+              < Route path="/perfil" element={< Perfil />} />
+              < Route path="/formulario" element={< Formulario />} />
+              < Route path="/galeria" element={< Galeria />} />
+              < Route path="/infoproducto" element={< InfoProducto />} />
+              < Route path="/favoritos" element={< Favoritos />} />
+              < Route path="/enventa" element={< EnVenta />} />
+              < Route path="*" element={<NotFound/>}/>
+            </ Routes >    
+        </ BrowserRouter >
+      </MyContext.Provider>      
     </div>
   );
 }
-
-export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../assets/img/back2.png";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className=" m-3 p-3">¡Bienvenido Conecta-2!</h1>
+      <img src={Logo} alt="marketplace" />
+    </div>
+  );
+};
+
+export default Home;
